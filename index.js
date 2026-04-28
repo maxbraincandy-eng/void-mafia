29e77bb416149e22f9e9c4f1b76e21bdc171786a
maxbraincandy-eng <maxbraincandy@gmail.com>
2026-04-28 06:27:20 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// აქ ჩასვი შენი MongoDB Connection String
-// <password>-ის ნაცვლად ჩაწერე შენი ნამდვილი პაროლი
-const mongoURI = "შენი_ლინკი_აქ"; 
+// შენი მონაცემები უკვე ჩავსვი აქ
+const mongoURI = "mongodb+srv://maxbraincandy_db_user:C8yIfHgHiNCCukBw@cluster0.enaxpdp.mongodb.net/mafia_db?retryWrites=true&w=majority&appName=Cluster0"; 
 
 mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB Connected...'))
-  .catch(err => console.log('Database Error:', err));
+  .then(() => console.log('MongoDB Connected Successfully!'))
+  .catch(err => console.log('Database Connection Error:', err));
 
 app.get('/', (req, res) => {
   res.send('Void Mafia Server is Running!');
 });
 
-// მარტივი ლოგიკა სოკეტებისთვის
+// მოთამაშეების დაკავშირების ლოგიკა
 io.on('connection', (socket) => {
   console.log('New Player Connected:', socket.id);
+  
+  socket.on('disconnect', () => {
+    console.log('Player Disconnected:', socket.id);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
