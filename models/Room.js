c8422d954c66b9461e3c721845b3d08855a9821f
maxbraincandy-eng <maxbraincandy@gmail.com>
2026-04-29 01:18:04 +0400

    Update Room.js

--- models/Room.js
+++ models/Room.js
@@ -1,7 +1,10 @@
-{
-  _id,
-  name,
-  createdBy,
-  players: [userId],
-  status: "waiting" | "playing" | "finished"
-}
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    status: { type: String, default: 'waiting' }
+});
+
+module.exports = mongoose.model('Room', roomSchema);
