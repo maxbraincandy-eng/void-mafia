6142f85e3e6dc121afa1a0b2e2cdb34e4c799b92
maxbraincandy-eng <maxbraincandy@gmail.com>
2026-05-02 16:43:11 +0400

    Add Counter model with schema definition

---
+++ src/models/Counter.js
@@ -0,0 +1,1 @@
+const mongoose=require('mongoose'); const CounterSchema=new mongoose.Schema({key:{type:String,unique:true},value:{type:Number,default:0}}); module.exports={Counter:mongoose.models.Counter||mongoose.model('Counter',CounterSchema)};
