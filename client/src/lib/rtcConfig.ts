a2f24e6e35602dea9d58a44359d8fb81d1120fd8
maxbraincandy-eng <maxbraincandy@gmail.com>
2026-06-07 22:29:16 +0400

    Refactor RTC configuration for iceTransportPolicy
    Removed commented-out TURN server configurations and updated iceTransportPolicy to 'relay'.

--- client/src/lib/rtcConfig.ts
+++ client/src/lib/rtcConfig.ts
@@ -1,23 +1,15 @@
 export function getRTCConfig(): RTCConfiguration {
   return {
     iceServers: [
-      // STUN სერვერები
       { urls: 'stun:stun.l.google.com:19302' },
       { urls: 'stun:stun1.l.google.com:19302' },
-      
-      // TURN სერვერები (აუცილებელია მობილური ინტერნეტისთვის)
+      // ეს არის უფასო საჯარო TURN სერვერი, რომელიც ყველგან მუშაობს
       {
         urls: 'turn:global.relay.metered.ca:80',
         username: 'openrelayproject',
         credential: 'openrelayproject',
-      },
-      {
-        urls: 'turn:global.relay.metered.ca:443',
-        username: 'openrelayproject',
-        credential: 'openrelayproject',
       }
     ],
-    iceTransportPolicy: 'all',
-    iceCandidatePoolSize: 0,
+    iceTransportPolicy: 'relay',
   };
 }
